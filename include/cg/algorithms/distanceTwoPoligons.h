92cb98531feec6e4a6f16656148566cd5fb880b7
Igor Kolobov <igorjan94@mail.ru>
2014-02-05 20:28:15 +0400

    distanceTwoPoligons with indexes

--- include/cg/algorithms/distanceTwoPoligons.h
+++ include/cg/algorithms/distanceTwoPoligons.h
@@ -25,74 +25,63 @@ namespace cg
     }
 
     template<class Scalar>
+    bool less(cg::point_2t<Scalar> const &a, cg::point_2t<Scalar> const &b, cg::point_2t<Scalar> const &c, cg::point_2t<Scalar> const &d)
+    {
+        return orientation(a, b, c, d) != CG_RIGHT;
+    }
+
+    template<class Scalar>
+    int next(cg::contour_2t<Scalar> const& a, int index)
+    {
+        return (index + 1) % (a.size() - 1);
+    }
+
+    template<class Scalar>
+    void updateAnswer(contour_2t<Scalar> const& contour, point_2t<Scalar> const& point, int& index, std::pair<point_2t<Scalar>, point_2t<Scalar>>& ans)
+    {
+        segment_2t<Scalar> seg(contour[index], contour[index + 1]);
+        auto pr = projection(point, seg);
+        if (pr >= 0 && pr <= 1)
+        {
+            auto proj = seg[0] + pr * (seg[1] - seg[0]);
+            if (compare_dist(proj, point, ans.first, ans.second))
+                ans = std::make_pair(proj, point);
+        }
+        index = next(contour, index);
+        if (compare_dist(contour[index], point, ans.first, ans.second))
+            ans = std::make_pair(contour[index], point);
+    }
+
+    template<class Scalar>
     std::pair<cg::point_2t<Scalar>, cg::point_2t<Scalar>> distanceTwoPoligons(contour_2t<Scalar>& a, contour_2t<Scalar>& b)
     {
         if (cg::orientation(a[0], a[1], a[2]) == CG_RIGHT)
             reverse(a);
         if (cg::orientation(b[0], b[1], b[2]) == CG_RIGHT)
             reverse(b);
-        auto p = std::min_element(a.begin(), a.end());
-        auto q = std::max_element(b.begin(), b.end());
-        auto next = [] (cg::contour_2t<Scalar> const& a, __gnu_cxx::__normal_iterator<const cg::point_2t<Scalar>*, std::vector<cg::point_2t<Scalar>, std::allocator<cg::point_2t<Scalar>>>> p)
-        {
-            ++p;
-            if (p == a.end())
-                p = a.begin();
-            return p;
-        };
-        auto less = [] (cg::point_2t<Scalar> const &a, cg::point_2t<Scalar> const &b,
-                        cg::point_2t<Scalar> const &c, cg::point_2t<Scalar> const &d)
-        {
-            return orientation(a, b, c, d) != CG_RIGHT;
-        };
+        int i, j, minimum, maximum;
+        for (int k = i = 0; k < a.size(); k++)
+            if (a[i] > a[k])
+                i = k;
+        for (int k = j = 0; k < b.size(); k++)
+            if (b[j] < b[k])
+                j = k;
+        minimum = i;
+        maximum = j;
 
-        auto min = p;
-        auto max = q;
-        auto p_n = p;
-        auto q_n = q;
-        p_n = next(a, p_n);
-        q_n = next(b, q_n);
-        std::pair<point_2t<Scalar>, point_2t<Scalar>> ans = std::make_pair(*p, *q);
+        std::pair<point_2t<Scalar>, point_2t<Scalar>> ans = std::make_pair(a[i], b[j]);
         do
         {
-            if (compare_dist(*p, *q, ans.first, ans.second))
-                ans = std::make_pair(*p, *q);
-            bool less1 = less(*p, *p_n, *q_n, *q);
-            bool less2 = less(*p, *p_n, *q, *q_n);
+            if (compare_dist(a[i], b[j], ans.first, ans.second))
+                ans = std::make_pair(a[i], b[j]);
+            bool less1 = less(a[i], a[i + 1], b[j + 1], b[j]);
+            bool less2 = less(a[i], a[i + 1], b[j], b[j + 1]);
             if (less1)
-            {
-                segment_2t<Scalar> seg(*p, *p_n);
-                auto pr = projection(*q, seg);
-
-                if (pr >= 0 && pr <= 1)
-                {
-                    auto proj = seg[0] + pr * (seg[1] - seg[0]);
-                    if (compare_dist(proj, *q, ans.first, ans.second))
-                        ans = std::make_pair(proj, *q);
-                }
-                p = p_n;
-                p_n = next(a, p_n);
-                if (compare_dist(*p, *q, ans.first, ans.second))
-                    ans = std::make_pair(*p, *q);
-            }
+                updateAnswer(a, b[j], i, ans);
             if (less2)
-            {
-                segment_2t<Scalar> seg(*q, *q_n);
-                auto pr = projection(*p, seg);
-                if (pr >= 0 && pr <= 1)
-                {
-                    auto proj = seg[0] + pr * (seg[1] - seg[0]);
-                    if (compare_dist(proj, *p, ans.first, ans.second))
-                        ans = std::make_pair(proj, *p);
-                }
-                q = q_n;
-                q_n = next(b, q_n);
-
-                if (compare_dist(*p, *q, ans.first, ans.second))
-                    ans = std::make_pair(*p, *q);
-            }
-      }
-      while (p != min || q != max);
-      return ans;
-   }
+                updateAnswer(b, a[i], j, ans);
+        }
+        while (i != minimum || j != maximum);
+        return ans;
+    }
 } //namespace cg;
